c2ac960eb87df6a9882615e800b75ac29ef9ddc1
leocjj <leocj@hotmail.com>
2019-09-19 18:23:06 +0000

    Task_3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 97; c < 123; c++)
+	{
+		putchar(c);
+	}
+	for (c = 65; c < 91; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
