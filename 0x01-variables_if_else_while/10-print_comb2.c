7d664a53c3fd5fbf6eb3143f4e57471bee78a57e
leocjj <leocj@hotmail.com>
2019-09-19 19:13:55 +0000

    Test_10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m, n;
+
+	for (m = 48; m < 58; m++)
+	{
+		for (n = 48; n < 58; n++)
+		{
+			putchar(m);
+			putchar(n);
+			if (m != 57 && n != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
