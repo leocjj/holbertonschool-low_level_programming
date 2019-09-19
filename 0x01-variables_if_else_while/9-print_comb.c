1e7215f18bfeeed3f2a47bfea9c33d6f11f1853e
leocjj <leocj@hotmail.com>
2019-09-19 18:56:29 +0000

    Task_9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		putchar(c);
+		if (c < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(13);
+	return (0);
+}
