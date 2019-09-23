696d667aa9cc1cb2d0eb02ec7394ad7c4feed4c4
leocjj <leocj@hotmail.com>
2019-09-23 21:05:40 +0000

    Task_0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  * main - Entry point
@@ -7,6 +7,11 @@
  */
 int main(void)
 {
-	printf("Holberton\n");
+	int i;
+	char a[] = {"Holberton\n"};
+
+	for(i =0; i <=9; i++)
+		_putchar(a[i]);
+
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,8 @@
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+extern int _putchar(char c);
