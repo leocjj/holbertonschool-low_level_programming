a191ae8ca1623ea4cf9c002091498409e3ef8289
leocjj <leocj@hotmail.com>
2019-09-24 15:39:58 +0000

    Task_7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,21 @@
  */
 int print_last_digit(int n)
 {
-	int temp;
+	int temp = n;
 
-	temp = n;
+	if (temp > 0)
+	{
+		_putchar(temp % 10 + '0');
+	}
+	if (temp == 0)
+	{
+		_putchar('0');
+	}
 	if (temp < 0)
-		temp = temp * (-1);
-	temp = temp % 10;
-	_putchar('0' + temp);
+	{
+		temp = -temp;
+		_putchar(temp % 10 + '0');
+	}
+
 	return (temp);
 }
