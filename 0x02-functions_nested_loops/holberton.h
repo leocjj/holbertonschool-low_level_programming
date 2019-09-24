bde92b3c093fd3fae914eafaf2c55dc47be7a64a
leocjj <leocj@hotmail.com>
2019-09-24 13:38:22 +0000

    Tast_1

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,8 +1,2 @@
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-extern int _putchar(char c);
+int _putchar(char c);
+void print_alphabet(void);
