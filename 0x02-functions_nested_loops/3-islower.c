982880347fee68f763cfbed228377220c61b08f9
leocjj <leocj@hotmail.com>
2019-09-24 14:02:24 +0000

    Task_3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 
 /**
  * _islower - check the code for Holberton School students.
+ * @c: The character to print
  *
  * Return: Always 0.
  */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	return (0);
 }
