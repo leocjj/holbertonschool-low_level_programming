40a2713c24e76656ea4650bc424cfafce4b9572f
leocjj <leocj@hotmail.com>
2019-09-24 19:22:41 +0000

    Task_10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - prints the last digit of a number.
+ * @n: The integer to sum.
+ * @m: The integer to sum.
+ *
+ * Return: sum of digit n with m.
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
