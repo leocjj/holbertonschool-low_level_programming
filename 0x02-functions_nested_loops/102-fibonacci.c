e4dffdfb5b1da0cd67585b5016d68653388e00a3
leocjj <leocj@hotmail.com>
2019-09-25 01:39:02 +0000

    Task_14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: void
+ */
+void main(void)
+{
+	unsigned long int i, n1, n2, temp;
+
+	n1 = 1;
+	n2 = 2;
+	temp = 0;
+	printf("%lu, ", n1);
+	for (i = 1; i <= 50; i++)
+	{
+		printf("%lu", n2);
+		if (i != 98)
+			printf(", ");
+		else
+			printf("\n");
+		temp = n2;
+		n2 = n1 + n2;
+		n1 = temp;
+	}
+}
