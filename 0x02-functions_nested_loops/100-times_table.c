0657337a8e343f1232543c2eab40cccbf8a719d4
leocjj <leocj@hotmail.com>
2019-09-25 00:08:48 +0000

    Task_12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,42 +8,43 @@
  */
 void print_times_table(int n)
 {
-	int i, j;
+	int i;
+	int j;
 
-	if (n >= 1 && n <= 15)
+	if ((n > 15) || (n < 0))
+		return;
+
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			if ((i * j) < 10)
 			{
-				if ((i * j) < 10)
-				{
-					if (j != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-				}
-				else if ((i * j) < 100)
+				if (j != 0)
 				{
 					_putchar(' ');
-					_putchar(i * j / 10 + '0');
-				}
-				else
-				{
-					_putchar(i * j / 100 + '0');
-					_putchar((i * j / 10) % 10 + '0');
-				}
-				_putchar(i * j % 10 + '0');
-				if (j == n)
-				{
-					_putchar('\n');
-				}
-				else
-				{
-					_putchar(',');
 					_putchar(' ');
 				}
+			}
+			else if ((i * j) < 100)
+			{
+				_putchar(' ');
+				_putchar(i * j / 10 + '0');
+			}
+			else
+			{
+				_putchar(i * j / 100 + '0');
+				_putchar((i * j / 10) % 10 + '0');
+			}
+			_putchar(i * j % 10 + '0');
+			if (j == n)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 	}
