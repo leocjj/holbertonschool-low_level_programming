ebc5de4bfcb6f32bb59f51d1e39f71eb68d5b94c
leocjj <leocj@hotmail.com>
2019-09-26 18:31:35 +0000

    Task_8

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = -10; i < 11; i++)
+		print_square(i);
+	return (0);
+}
