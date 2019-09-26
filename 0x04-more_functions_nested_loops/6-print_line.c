95b215b6dd4a9f452a7b9ad878f2d015090ed9e3
leocjj <leocj@hotmail.com>
2019-09-26 17:58:53 +0000

    Task_6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,25 @@
 #include "holberton.h"
 
 /**
- * print_line - Entry point
- * @n: length of the line
+ * print_diagonal - Entry point
+ * @n: length of
  *
  * Return: void
  */
-void print_line(int n)
+void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 0)
+	{
 		for (i = 1; i <= n; i++)
-			_putchar('_');
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+		}
+	}
 	_putchar('\n');
 }
