dd700d8a8818f66844afb1295a99165dfcb48aba
leocjj <leocj@hotmail.com>
2019-09-26 18:21:33 +0000

    Task_7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Entry point
+ * @n: length of the diagonal
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
