2bd626ef728ee33e6bb2e69fcde468fba2bf2316
leocjj <leocj@hotmail.com>
2019-09-26 14:54:57 +0000

    Task_0

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 127; c++)
+		printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
