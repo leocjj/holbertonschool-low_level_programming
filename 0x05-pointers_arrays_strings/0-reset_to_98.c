887bb33c90a58555a1843fab5760ba2e98a93d18
leocjj <leocj@hotmail.com>
2019-09-30 19:54:19 +0000

    Task_01

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - change the value of an integer using a pointer to it.
+ * @n: pointer to an integer passed as parameter.
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
