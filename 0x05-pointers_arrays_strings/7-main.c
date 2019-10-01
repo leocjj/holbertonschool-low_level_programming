acd4bfe04ee0f15116b1cc06ed9f905dd806d968
leocjj <leocj@hotmail.com>
2019-10-01 19:20:37 +0000

    Task_7

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+
+	str = "0123456789A";
+	puts_half(str);
+
+	return (0);
+}
