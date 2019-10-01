a85e2d3b1a6d0bc6bdb0760cc31c02804cae712b
leocjj <leocj@hotmail.com>
2019-10-01 19:23:46 +0000

    Task_7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,12 +15,16 @@ void puts_half(char *str)
 	/* i will be the legth of str. Counts until *str == '\0'*/
 	while (*str++)
 		i++;
-	/* if legth is pair, return str to the half of the string.
-	   + 1 because in counting, i went to '\n' character inclusive*/
+	/*
+	 * if legth is pair, return str to the half of the string.
+	 *  + 1 because in counting, i went to '\n' character inclusive
+	 */
 	if (i % 2 == 0)
 		str -= i / 2 + 1;
-	/* if lecth is odd, return str to half minus 1. Additional -1 is
-	   because of i went to '\n' when counting */
+	/*
+	 * if lecth is odd, return str to half minus 1. Additional -1 is
+	 * because of i went to '\n' when counting
+	 */
 	else
 		str -= (i + 1) / 2 + 1;
 	/* print str with putchar and POST-increment.*/
