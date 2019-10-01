e01e5f24c1959a8c36839fb32df09ef4ee232427
leocjj <leocj@hotmail.com>
2019-10-01 15:23:29 +0000

    Task_6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _put2s - prints every other character of a string passed as pointer.
+ * puts2 - prints every other character of a string passed as pointer.
  * @str: pointer to a string passed as parameter.
  *
  * Return: void
@@ -11,7 +11,7 @@ void puts2(char *str)
 	while (*str != '\0')
 	{
 		_putchar(*str);
-		str +=2;
+		str += 2;
 	}
 	_putchar('\n');
 }
