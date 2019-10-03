1dcfd31c7ee04b26e51a4e3dc346542c4886f4d4
leocjj <leocj@hotmail.com>
2019-10-03 20:46:14 +0000

    Task_6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,20 +8,25 @@
  */
 char *cap_string(char *s)
 {
-	int i, j;
-	char cond[] = { ' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}' };
+	int i, j, flag;
+	char cond[] = { 32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125 };
 
 	i = 0;
+	flag = 0;
 	while (*(s + i))
 	{
-
 		for (j = 0; j <= 12; j++)
 			if (*(s + i) == cond[j])
-			{
-				if (*(s + i + 1) >= 'a' && *(s + i + 1) <= 'z')
-					*(s + i + 1) -= 32;
-				break;
-			}
+				flag = 1;
+		if (*(s + i) >= 'A' && *(s + i) <= 'Z')
+			flag = 0;
+		else if (*(s + i) >= '0' && *(s + i) <= '9')
+			flag = 0;
+		else if (*(s + i + 1) >= 'a' && *(s + i + 1) <= 'z' && flag)
+		{
+			*(s + i + 1) -= 32;
+			flag = 0;
+		}
 		i++;
 	}
 
