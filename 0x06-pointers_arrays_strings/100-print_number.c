721145901b2b2f750ac2786f9c7c07620b65561e
leocjj <leocj@hotmail.com>
2019-10-04 20:22:27 +0000

    Task_9

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * print_number - Entry point
+ * @n: integer to print
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int max, a;
+
+	max = 1000000000;
+	if (n < 0)
+	{
+		_putchar('-');
+		a = n / max;
+		_putchar(-a + '0');
+		n %= max;
+		n *= -1;
+		max /= 10;
+	}
+	while (max - n > 0)
+	{
+		max /= 10;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (max >= 1)
+	{
+		a = n / max;
+		_putchar (a + '0');
+		n -= max * a;
+		max /= 10;
+	}
+}
