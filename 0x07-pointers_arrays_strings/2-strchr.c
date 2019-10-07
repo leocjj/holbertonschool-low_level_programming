6c13d490ffef8b7db82a47923bb62289542acfeb
leocjj <leocj@hotmail.com>
2019-10-07 18:13:00 +0000

    Task_2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to source
+ * @c: byte to look for in source
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	return (0);
+}
