ac9127764ed26f30630b8a3660d6683a0e5f57bd
leocjj <leocj@hotmail.com>
2019-10-07 20:06:35 +0000

    Task_4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,18 +11,18 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
-        i = 0;
-        while (*(s + i))
-        {
-                j = 0;
-                while (*(accept + j))
-                {
-                        if (*(s + i) == *(accept + j))
-                                return (s + i);
-                        j++;
-                }
+	i = 0;
+	while (*(s + i))
+	{
+		j = 0;
+		while (*(accept + j))
+		{
+			if (*(s + i) == *(accept + j))
+				return (s + i);
+			j++;
+		}
 		i++;
-        }
+	}
 
-        return (NULL);
+	return (NULL);
 }
