354d0a048bab14b8fb4632b34c42d1d75210e81c
leocjj <leocj@hotmail.com>
2019-10-08 03:29:00 +0000

    Task_7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,16 +9,20 @@
 void print_chessboard(char (*a)[8])
 {
 	unsigned int i, j;
+	char *p;
 
 	i = 0;
 	j = 0;
 	while (i < 8)
 	{
+		p = a[i];
+		j = 0;
 		while (j < 8)
 		{
-			_putchar(*a[i + j]);
+			_putchar(*(p + j));
 			j++;
 		}
+		_putchar('\n');
 		i++;
 	}
 }
