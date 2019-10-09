f2747c5a579ca8ec058f633d194a7eb007019631
leocjj <leocj@hotmail.com>
2019-10-09 19:34:52 +0000

    Task_7

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -12,12 +12,13 @@ char *last(char *s)
 	if (*s == '\0')
 		return (s - 1);
 	else
-		return(last (s + 1));
+		return (last(s + 1));
 }
 
 /**
  * is_pal - evaluates a palinddrome string
  * @s: strint to evaluate
+ * @ss: strint to evaluate
  *
  * Return: returns 1 if the string is palindrome, otherwise return 0
  */
@@ -29,11 +30,11 @@ int is_pal(char *s, char *ss)
 	if (s >= ss)
 		return (1);
 	else
-		return(is_pal (s + 1, ss - 1));
+		return (is_pal(s + 1, ss - 1));
 }
 /**
  * is_palindrome - evaluates if the string is palindrome
- * @c: string
+ * @s: string
  *
  * Return: returns 1 if the string is palindrome, otherwise return 0
  */
