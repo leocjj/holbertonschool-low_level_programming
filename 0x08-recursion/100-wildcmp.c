ea94940e2051c5a9d4c6e6fe087e42f6604d3f7c
leocjj <leocj@hotmail.com>
2019-10-09 19:55:23 +0000

    Task_8

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s: The string to print
+ *
+ * Return: 1 if the strings can be considered identical, otherwise return 0
+ * The special char * can replace any string (including an empty string)
+ */
+int wildcmp(char *s1, char *s2)
+{
+	
+}
