1604bdd071e6506870d2669709d7bd0a26d8e8dc
leocjj <leocj@hotmail.com>
2019-10-10 20:08:41 +0000

    Task_8

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * wildcmp - compares two strings
  * @s: The string to print
@@ -9,5 +8,18 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return( wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return(wildcmp(s1, s2 + 1));
+		else if (wildcmp (s1, s2 + 1))
+			return (1);
+		else
+			return(wildcmp(s1 + 1, s2));
+	}
+	return (0);
 }
