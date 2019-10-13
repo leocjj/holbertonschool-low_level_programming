6d41a295c37a8c400656318f70c6ec19e3b46056
leocjj <leocj@hotmail.com>
2019-10-13 02:18:00 +0000

    Task_2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of pointer with arguments for main function.
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(int argc, char **argv)
 {
 	int i = 0;
-	while(i < argc)
+
+	while (i < argc)
 	{
 		printf("%s\n", *(argv + i));
 		i++;
