4e92dc01b40ff171c4507d5c74e599269552b781
leocjj <leocj@hotmail.com>
2019-10-16 01:46:37 +0000

    Task_6

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * strtow - function that splits a string into words.
+ * @str: string to split
+ *
+ * Return: array of strings.
+ */
+
+char **strtow(char *str)
+{
+	char **result = NULL;
+
+	if (str == NULL)
+		return (NULL);
+
+
+	return result;
+}
