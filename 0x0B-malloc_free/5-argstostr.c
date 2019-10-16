2f40e17f408ecbefa06329a9f4695d22c7f02a12
leocjj <leocj@hotmail.com>
2019-10-16 01:36:37 +0000

    Task_5

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -17,16 +17,16 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (i = 0; i < ac; i++)
+	for (i = 1; i < ac; i++)
 		for (j = 0; av[i][j] != '\0'; j++)
 			size++;
 
-	result = malloc((size + ac) * sizeof(char *));
+	result = malloc((size + ac + 1) * sizeof(char));
 	if (result == NULL)
 		return (NULL);
 
 	size = 0;
-	for (i = 0; i < ac; i++)
+	for (i = 1; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++, size++)
 			result[size] = av[i][j];
