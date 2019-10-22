0c3961a0047bb1332af860365e74be72ed3661b0
leocjj <leocj@hotmail.com>
2019-10-22 04:22:32 +0000

    Task_4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,27 +21,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog_copy = malloc(sizeof(dog_t));
 	if (dog_copy == NULL)
-		return NULL;
+		return (NULL);
 
-	(*dog_copy).name = malloc((len_name + 1) * sizeof(char));
-	if ((*dog_copy).name == NULL)
+	dog_copy->name = malloc((len_name + 1) * sizeof(char));
+	if (dog_copy->name == NULL)
 	{
 		free(dog_copy);
 		return (NULL);
 	}
-	(*dog_copy).owner = malloc((len_owner + 1) * sizeof(char));
-	if ((*dog_copy).owner == NULL)
+	dog_copy->owner = malloc((len_owner + 1) * sizeof(char));
+	if (dog_copy->owner == NULL)
 	{
-		free((*dog_copy).name);
+		free(dog_copy->name);
 		free(dog_copy);
 		return (NULL);
 	}
 
 	for (i = 0; i <= len_name; i++)
-		*((*dog_copy).name + i) = *(name + i);
-	for (i = 0; i < len_owner; i++)
-		*((*dog_copy).owner + i) = *(owner + i);
-	(*dog_copy).age = age;
+		*(dog_copy->name + i) = *(name + i);
+	for (i = 0; i <= len_owner; i++)
+		*(dog_copy->owner + i) = *(owner + i);
+	dog_copy->age = age;
 
 	return (dog_copy);
 }
