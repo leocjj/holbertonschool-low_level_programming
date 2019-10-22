f83d4daea3c2cbb33a1327dc60ffb69fee06d6b7
leocjj <leocj@hotmail.com>
2019-10-22 04:05:04 +0000

    Task_4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * new_dog - creates a new dog.
@@ -11,14 +12,27 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t dog;
+	int i = 0, len_name = 0, len_owner = 0;
+	char *name_copy, *owner_copy;
+	dog_t *dog_copy;
 
-	if (name != NULL && owner != NULL)
-	{
-		dog->name = name;
-		dog->age = age;
-		dog->owner = owner;
-	}
-	else
-		return NULL;
+	while (*(name + len_name) != '\0')
+		len_name++;
+	while (*(owner + len_owner) != '\0')
+		len_owner++;
+
+	dog_copy = malloc(sizeof(dog_t));
+	name_copy = malloc(len_name * sizeof(char));
+	owner_copy = malloc(len_owner * sizeof(char));
+	if (dog_copy == NULL || name_copy == NULL || owner_copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < len_name; i++)
+		*(name_copy + i) = *(name + i);
+	for (i = 0; i < len_owner; i++)
+		*(owner_copy + i) = *(owner + i);
+	(*dog_copy).name  = name_copy;
+	(*dog_copy).age = age;
+	(*dog_copy).owner  = owner_copy;
+	return (dog_copy);
 }
