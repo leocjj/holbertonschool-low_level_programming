5b41ef8ff6d18a6e819440c27d3e231bfe0ae99b
leocjj <leocj@hotmail.com>
2019-10-23 20:38:37 +0000

    Task_3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*(argv + 2) == '/' || *(argv + 2) == %) && b == 0)
+	if ((*(*(argv + 2)) == '/' || *(*(argv + 2)) == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
