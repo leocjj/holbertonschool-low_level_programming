470cd83c8e5c5a0e429a5cda779b11c30a5a287f
leocjj <leocj@hotmail.com>
2019-10-23 19:41:20 +0000

    Task_3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef CALC_H_INCLUDED
+#define CALC_H_INCLUDED
 
 /**
  * struct op - Struct op
@@ -19,6 +19,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-
 
 #endif
