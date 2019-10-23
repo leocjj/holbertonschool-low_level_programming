1e5ac1eb0b7d1b76c5a1f06367a5ca02e61bea64
leocjj <leocj@hotmail.com>
2019-10-23 19:40:54 +0000

    Task_3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function that add two numbers.
+ * @a: first parameter.
+ * @b: second parameter.
+ *
+ * Return: result of the operation.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function that substrac two numbers.
+ * @a: first parameter.
+ * @b: second parameter.
+ *
+ * Return: result of the operation.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function that multiply two numbers.
+ * @a: first parameter.
+ * @b: second parameter.
+ *
+ * Return: result of the operation.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function that divides two numbers.
+ * @a: first parameter.
+ * @b: second parameter.
+ *
+ * Return: result of the operation.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function that calculate modulus of two numbers.
+ * @a: first parameter.
+ * @b: second parameter.
+ *
+ * Return: result of the operation.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
