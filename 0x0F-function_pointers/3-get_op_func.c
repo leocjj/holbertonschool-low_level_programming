3ba8414e817455d6f43a5e48f6e8d3ee8e44dca6
leocjj <leocj@hotmail.com>
2019-10-23 19:41:08 +0000

    Task_3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - function select corresponding function.
+ * @s: operator passed as parameter to select function.
+ *
+ * Return: pointer to function that correspond to s.
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i <= 5)
+	{
+		if (*(ops[i].op) == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
