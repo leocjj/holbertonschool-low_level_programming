604f2cb104ba7b0d96d5e077df1e3ac17be93f36
leocjj <leocj@hotmail.com>
2019-10-25 23:49:45 +0000

    test

--- temp/printf/_printf.c
+++ temp/printf/_printf.c
@@ -4,35 +4,45 @@
 #include "holberton.h"
 
 /**
- * _printf - function that prints formated text in standard output
+ * _printf - function that prints formated text in standard output.
  * @format: text and format to be printed.
  *
- * Return: number of characters printed.
+ * Return: number of characters printed
  */
 
 int _printf(const char *format, ...)
 {
         va_list args;
-        int i, characters_printed;
+        int i = 0, count = 0, chars_printed = 0;
         char *buffer;
 
 	buffer = malloc(1024);
-        va_start (args, format);
+        va_start(args, format);
 
-        for (i = 0; *format != '\0'; format++)
+        for (i = 0; *(format + i) != '\0'; i++)
 	{
-		if (*(format) == '%')
-			if (*(format - 1) != '\\')
+		if (*(format + i) == '%')
+			if (*(format + i - 1) != '\\')
 			{
-				char *temp = checkformat(&format , args);
-				buffer = concatenate(buffer, temp);
+				count = 1;/*count_id(format + i);*/
+                                if (count == 0)
+					break;
+/*char *temp = checkformat(format + i, args);*/
+				if(*(format + i + 1) == 'c')
+					buffer = concatenate(buffer, va_arg(args, char *));
 			}
 			else
+			{
 				buffer = concatenate(buffer, "%");
+				i++;/* Jump over the second % */
+			}
+
 		else
-			buffer = concatenate(buffer, *format);
+			*(buffer + i) = *(format + i);/* Adds a char to buffer */
 	}
 
+	write(1, buffer, i);
+
 	va_end (args);
-        return (characters_printed);
+        return (chars_printed);
 }
