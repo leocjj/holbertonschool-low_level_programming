5b5faa13096cd2dae85fe985d2d554dd0d81b153
Holberton <Holberton@CAL-0020.hbtn.io>
2019-10-25 18:44:32 -0500

    Update count_id.c

--- temp/printf/count_id.c
+++ temp/printf/count_id.c
@@ -5,19 +5,23 @@
 
 /**
  * *count_id - Function that return the length of the parameter.
- * @count1: Length of the printf argument.
+ * @count: Length of the printf argument.
  * Return: Return the length of the argument.
  */
 
-char *count_id(char *count1)
+int count_id(char *count)
 {
-	int i, j;
-	char letters[] = "c, s, d, e, f, g, i, o, s, u, k, x, p, X, G, F, E"
+	int i, j, k, c;
+	char letters[] = "c, s, d, e, f, g, i, o, s, u, k, x, p, X, G, F, E";
 
-	if (count1 == NULL)
-		return "";
-	for (i = 0; i  != letters, i++)
-		if (count1 == " ")
-			return NULL;
+	if (count == NULL)
+		return NULL;
+	for (k = 0; count[k] != '\0', k++)
+		{
+		}
+	for (i = 1; count[i] != '%', i++)
+		for (j = 0; j < k; j++)
+			if (count[i] != letter[i])
+			c++;
 	return (i);
 }
