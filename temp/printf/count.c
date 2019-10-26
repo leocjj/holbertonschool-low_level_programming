afa6befdefcfdb0c4d78b90471bc4baf410952e8
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-10-25 19:39:16 -0500

    version06

--- temp/printf/count.c
+++ temp/printf/count.c
@@ -11,13 +11,15 @@
 
 int count_id(const char *count)
 {
-	int i = 0, j = 0;
+	int i = 0, j = 0, size_letters = 0;
 	char letters[] = "csdefgiosukxpXGFE";
 
 	if (count == NULL)
 		return NULL;
+	for (size_letters = 0; letters[size_letters] != '\0'; size_letters++)
+		;
 	for (i = 1; count[i] != '\0'; i++)
-		for (j = 0; j < 17; j++)
+		for (j = 0; j < size_letters; j++)
 			if (count[i] == letters[j])
 				return (i);
 	return (0);
--- temp/printf/count_id.c
+++ temp/printf/count_id.c
@@ -9,19 +9,18 @@
  * Return: Return the length of the argument.
  */
 
-int count_id(char *count)
+int count_id(const char *count)
 {
-	int i, j, k, c;
-	char letters[] = "c, s, d, e, f, g, i, o, s, u, k, x, p, X, G, F, E";
+	int i = 0, j = 0, size_letters = 0;
+	char letters[] = "csdefgiosukxpXGFE";
 
 	if (count == NULL)
 		return NULL;
-	for (k = 0; count[k] != '\0', k++)
-		{
-		}
-	for (i = 1; count[i] != '%', i++)
-		for (j = 0; j < k; j++)
-			if (count[i] != letter[i])
-			c++;
-	return (i);
+	for (size_letters = 0; letters[size_letters] != '\0'; size_letters++)
+		;
+	for (i = 1; count[i] != '\0'; i++)
+		for (j = 0; j < size_letters; j++)
+			if (count[i] == letters[j])
+				return (i);
+	return (0);
 }
