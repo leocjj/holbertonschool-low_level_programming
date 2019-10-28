1a5133952ac9044ab5a17a15de955f314eb9a3d9
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-10-28 15:46:04 -0500

    v48

--- printf/concat2.c
+++ printf/concat2.c
@@ -14,7 +14,8 @@
 
 int concat_b(char *buffer, unsigned int integer, int *chars_printed)
 {
-	int power = 1, result = 0;
+	int result = 0;
+	unsigned long int power = 1, binary = 0;
 
 	if (integer == 0)
 	{
@@ -23,10 +24,41 @@ int concat_b(char *buffer, unsigned int integer, int *chars_printed)
 	}
 	while (integer / 2 > 0)
 	{
-		result += (integer % 2) * power;
+		binary += (integer % 2) * power;
 		integer /= 2;
 		power *= 10;
 	}
-	result += concat_i(buffer, result, chars_printed);
+	result += concat_l(buffer, binary, chars_printed);
+	return (result);
+}
+
+/**
+ * concat_l - Function that concatenate a strings with long int.
+ * @buffer: string base to add the next parameter.
+ * @integer: unsigned long integer to add.
+ * @chars_printed: chars printed.
+ *
+ * Return: number of character added.
+ */
+
+int concat_l(char *buffer, unsigned long int integer, int *chars_printed)
+{
+	int result = 0;
+	unsigned long int max = 10000000000000000000, temp = 0;
+
+	if (integer == 0)
+	{
+		result += concat_c(buffer, '0', chars_printed);
+		return (result);
+	}
+	while (integer / max == 0)
+		max /= 10;
+	while (max >= 1)
+	{
+		temp = integer / max;
+		result += concat_c(buffer, temp + '0', chars_printed);
+		integer -= max * temp;
+		max /= 10;
+	}
 	return (result);
 }
