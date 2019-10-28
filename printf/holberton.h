81773618be1ff88981d6119d04fab4ffe43d7968
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-10-27 22:21:59 -0500

    v18

--- printf/holberton.h
+++ printf/holberton.h
@@ -10,7 +10,7 @@ int count_id(const char *count1);
 int size_temp(char *temp_s);
 void free_temp(char *temp_s);
 void concat_c(char *buffer, va_list args);
-char *concat(char *s1, char *s2);
+int concat(char *s1, char *s2);
 
 
 #endif
