95b5364a37cf2251a8f5a716f858fde0d701b755
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-10-29 16:51:29 -0500

    v82

--- printf3/concat3.c
+++ printf3/concat3.c
@@ -126,7 +126,7 @@ int concat_u(char *buffer, int integer, int *chars_printed)
 	}
 	else
 	{
-		temp = UINT_MAX - integer + 1);
+		temp = UINT_MAX - integer + 1;
 		result += concat_i(buffer, temp, chars_printed);
 		return (result);
 	}
