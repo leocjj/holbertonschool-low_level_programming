32b56b84fc97679f972de52ce740c30465b5fd61
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-10-29 13:22:10 -0500

    v74

--- printf3/concat3.c
+++ printf3/concat3.c
@@ -78,21 +78,20 @@ char *rot13(char *string)
 	result = malloc(sizeof(char) * (string_size + 1));
 	if (result == NULL)
 		return ('\0');
-	free_temp(result);
 
-	for (result_size = 0; result_size < string_size; result_size++)
+	for (result_size = 0; result_size <= string_size; result_size++)
 	{
 		ch = string[result_size];
 		if (ch >= 'a' && ch <= 'z')
 		{
 			ch = ch - 'a' + 13;
-			if (ch >= 26)
+			if (ch > 26)
 				ch %= 26;
 			result[result_size] = (ch + 'a');
 		}else if (ch >= 'A' && ch <= 'Z')
 		{
 			ch = ch - 'A' + 13;
-			if (ch >= 26)
+			if (ch > 26)
 				ch %= 26;
 			result[result_size] = (ch + 'A');
 		}else
