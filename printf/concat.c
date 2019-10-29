b671d7593a433e730afc5dcf0f3a11db4e43a02b
michelalejo <michelmolina65@gmai.com>
2019-10-29 02:03:14 +0000

    concat-concat3

--- printf/concat.c
+++ printf/concat.c
@@ -132,9 +132,10 @@ int concat(char *s1, char *s2, int *chars_printed)
 	if (size_of_s1 + size_of_s2 + 1 > buffer_size)
 	{
                 temp = concat_s(s1,s2);
-		*chars_printed += write(1, temp, size_of_s1 + size_of_s2 + 1);
+		*chars_printed += write(1, temp, size_of_s1 + size_of_s2);
 		free(temp);
 		free_temp(s1);
+                size_of_s1 = 0;
 		size_of_s1 = 0;
 	}
 
--- printf/concat3.c
+++ printf/concat3.c
@@ -14,36 +14,24 @@
 
 char *concat_s(char *s1, char *s2)
 {
-	int j, i, k, l;
-	char *arr;
+	char *a = NULL;
+	unsigned int size1 = 0, size2 = 0, i = 0;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-        for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-	arr = malloc(sizeof(*arr) * (i + j + 1));
-	if (arr == NULL)
-	{
-		return ('\0');
-	}
-	for (k = 0; k < i; k++)
-	{
-		arr[k] = s1[k];
-	}
-	for (l = 0; l < j; l++)
-	{
-		arr[i + l] = s2[l];
-	}
-	*(arr + i + j) = '\0';
-	return (arr);
+	if (s1 != NULL)
+		for (i = 0; *(s1 + i) != '\0'; i++)
+			size1++;
+	if (s2 != NULL)
+		for (i = 0; *(s2 + i) != '\0'; i++)
+			size2++;
+
+	a = (char *) malloc((size1 + size2 + 1) * sizeof(char));
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < size1; i++)
+		*(a + i) = *(s1 + i);
+	for (i = 0; i < size2; i++)
+		*(a + size1 + i) = *(s2 + i);
+	*(a + size1 + size2) = '\0';
+	return (a);
 }
