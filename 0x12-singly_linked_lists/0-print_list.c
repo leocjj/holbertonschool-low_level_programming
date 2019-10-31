4e886d5754aa429a7c3cbac254b0a65359b051c3
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-10-31 14:51:11 -0500

    Task 00

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,17 @@ size_t print_list(const list_t *h)
 {
 	int n = 0;
 
+	if (h == NULL)
+		return (0);
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[5] %s\n", h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		n++;
 	}
-
 	return (n);
 }
