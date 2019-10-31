52a8a5e0ed01a89f6e48f661f68837d22a208f3b
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-10-31 16:02:53 -0500

    Task 02

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string>
+#include "lists.h"
+
+/**
+ * slen - function that returns the length of a string.
+ * @str: string to be length
+ *
+ * Return: length of the string.
+ */
+int slen(const char *str)
+{
+	int s = 0;
+
+	if (str = NULL)
+		return (0);
+	while (*str != '\0')
+		s++;
+}
+
+/**
+ * add_node - unction that adds a new node at the beginning of a list_t list.
+ * @h: structure of type list_t
+ *
+ * Return: the number of elements.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int s = 0;
+	list_t *p;
+
+	p = malloc(sizeof(list_t));
+	if (p == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+	}
+
+	*(p).str = strdup(str);
+	*(p).len = lsen(str);
+	*(p).next = *head;
+	*head = p;
+
+	free(p->str);
+	free(p);
+	return (*head);
+}
