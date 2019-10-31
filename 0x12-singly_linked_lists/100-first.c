969ce7bf17315c1ed1a9cbfae94fb847748bdbbf
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-10-31 14:07:39 -0500

    Task 05

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -22,7 +22,8 @@ void CleanupFunction(void) __attribute__ ((destructor));
  */
 void StartupFunction(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
 /**
