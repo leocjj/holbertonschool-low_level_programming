0cede8cd86a4847b78f0386d230adc15c8f2e0f0
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-11-04 23:29:36 -0500

    Task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
+
+void reverse_listint2(listint_t *curr, listint_t *prev, listint_t **head)
+{
+	listint_t *next = curr->next;
+	if (!curr->next)
+	{
+		*head = curr;
+		curr->next = prev;
+		return;
+	}
+
+	curr->next = prev;
+	reverse_listint2(next, curr, head);
+}
+
+/**
+ * reverse_listint - function that reverses a listint_t linked list.
+ * @head: address of a pointer to a structure of type listint_t
+ *
+ * Return: a pointer to the first node of the reversed list.
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	if (head == NULL)
+		return (NULL);
+	reverse_listint2(*head, NULL, head);
+	return (*head);
+}
