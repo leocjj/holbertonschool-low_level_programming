dfffed1e263ea081e05d4d1a6fdfa77960e4bd5e
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-11-05 22:58:36 -0500

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,32 +1,6 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * reverse_listint2 - function that reverses a listint_t linked list.
- * @head: address of a pointer to a structure of type listint_t
- * @prev: previus node.
- * @curr: current node.
- *
- * Return: a pointer to the first node of the reversed list.
- */
-void reverse_listint2(listint_t *curr, listint_t *prev, listint_t **head)
-{
-	listint_t *next = curr->next;
-
-	if (head == NULL)
-		return;
-
-	if (!curr->next)
-	{
-		*head = curr;
-		curr->next = prev;
-		return;
-	}
-
-	curr->next = prev;
-	reverse_listint2(next, curr, head);
-}
-
-/**
  * reverse_listint - function that reverses a listint_t linked list.
  * @head: address of a pointer to a structure of type listint_t
  *
@@ -35,8 +9,15 @@ void reverse_listint2(listint_t *curr, listint_t *prev, listint_t **head)
 
 listint_t *reverse_listint(listint_t **head)
 {
-	if (head == NULL)
-		return (NULL);
-	reverse_listint2(*head, NULL, head);
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
 	return (*head);
 }
