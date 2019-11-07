74032ee281de5138ed9c3cbefa1f7e2757b116dd
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-11-07 13:25:59 -0500

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,6 +12,10 @@ void print_binary(unsigned long int n)
 {
 	unsigned long int bit = 1, result = 0;
 	int bits_of_n = sizeof(n) * 8;
+
+	/**
+	 * Puts as '1' at the left of the max number
+	 */
 	bit = bit << (bits_of_n - 1);
 
 	if (n == 0)
@@ -20,11 +24,15 @@ void print_binary(unsigned long int n)
 		return;
 	}
 
+	/**
+	 * Move the '1' to the right until the first '1' of n is founded.
+	 */
 	while (!(n & bit))
-	{
 		bit = bit >> 1;
-	}
 
+	/**
+	 * Prints bit by bit of n, moving the '1' to the right each time.
+	 */
 	while (bit)
 	{
 		result = n & bit;
@@ -34,5 +42,4 @@ void print_binary(unsigned long int n)
 			_putchar('0');
 		bit = bit >> 1;
 	}
-
 }
