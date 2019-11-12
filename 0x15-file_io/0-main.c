c27e956670266f4f5bd769f11a64054c019b5a1f
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-11-12 18:03:07 -0500

    Readme, headers and Task 0

---
+++ 0x15-file_io/0-main.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+    ssize_t n;
+
+    if (ac != 2)
+    {
+        exit(1);
+    }
+    n = read_textfile(av[1], 114);
+    printf("\n(printed chars: %li)\n", n);
+    n = read_textfile(av[1], 1024);
+    printf("\n(printed chars: %li)\n", n);
+    return (0);
+}
---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,62 @@
+#include "holberton.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file to read.
+ * @letters: amount of character to read from file.
+ *
+ * Return: number of characters printed.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = -1;
+	size_t letters_readed = 0, letters_printed = 0;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	letters_readed = read(fd, buffer, letters);
+	if (letters_readed < letters)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	letters_printed = write(STDOUT_FILENO, buffer, strlen(buffer));
+	if (letters_printed < letters)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	close(fd);
+	free(buffer);
+
+	return (letters);
+}
