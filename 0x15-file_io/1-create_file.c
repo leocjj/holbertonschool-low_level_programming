c1738e94c9e1d601ffa3d716fcb72fb0c99e8151
leocjj <leocj@hotmail.com>
2019-11-13 00:23:49 +0000

    Task_01

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd = -1, text_size = 0;
+	int fd = -1, text_size = 0, i = 0;
 	char *c = '\0';
 	ssize_t letters_printed = 0;
 
@@ -30,8 +30,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (*text_content != '\0')
+		while (*(text_content + i) != '\0')
+		{
 			text_size++;
+			i++;
+		}
 	}
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
