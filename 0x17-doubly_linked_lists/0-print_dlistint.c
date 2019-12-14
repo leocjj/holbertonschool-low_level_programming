b9a708f5b9ed57c822056e5e05c76de337987d52
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-12-14 14:07:03 -0500

    Readme

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: structure of type list_t
+ *
+ * Return: the number of nodes printed.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int n = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,40 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+
+int _putchar(char c);
+size_t print_dlistint(const dlistint_t *h);
+
+
+/**
+ * StartupFunction - Apply constructor attribute. Is executed before main()
+ *
+ * Return: void
+ */
+void StartupFunction(void) __attribute__ ((constructor));
+
+/**
+ * StartupFunction - Apply the destructor attribute. Is executed after main()
+ *
+ * Return: void
+ */
+void CleanupFunction(void) __attribute__ ((destructor));
+
+
+#endif
