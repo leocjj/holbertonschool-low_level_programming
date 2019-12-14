5d86b41fa5744855845cd26311993201533e59f6
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-12-14 15:06:21 -0500

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,26 +4,9 @@
 #include "lists.h"
 
 /**
- * slen - function that returns the length of a string.
- * @str: string to be length
- *
- * Return: length of the string.
- */
-int slen(const char *str)
-{
-	int s = 0;
-
-	if (str == NULL)
-		return (0);
-	while (*(str + s) != '\0')
-		s++;
-	return (s);
-}
-
-/**
  * add_dnodeint - adds a new node at the beginning of a dlistint_t.
  * @head: addres of a pointer to a structure of type dlistint_t
- * @str: string to add to node.
+ * @n: integer to add to node.
  *
  * Return: pointer to structure of type dlistint_t.
  */
