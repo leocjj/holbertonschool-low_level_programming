54e8760d63bce67b106581983281a5f0d0a15908
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-12-15 15:53:15 -0500

    Task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -5,15 +5,12 @@
  * @head: pointer to a structure of type dlistint_t
  * @index: index of the node, starting at 0
  *
- * Return: the nth node of a listint_t linked list.
+ * Return: the nth node of a dlistint_t linked list.
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int counter = 0;
-	listint_t *head_copy = head;
-
-	if (head_copy == NULL)
-		return (NULL);
+	dlistint_t *head_copy = head;
 
 	while (head_copy != NULL)
 	{
