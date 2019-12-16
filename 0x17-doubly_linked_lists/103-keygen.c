5bc2b7de281302d99621347e414e3007ddb9ecb4
leocjj <leocj@hotmail.com>
2019-12-16 04:59:34 +0000

    T

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - function returns the number of elements in linked dlistint_t
+ * @h: structure of type dlistint_t
+ *
+ * Return: the number of elements.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n;
+
+	for (n = 0; h != NULL; n++)
+		h = h->next;
+
+	return (n);
+}
