b2187fe6e9d4bef55907ee5ed8ab73508ce4008b
leojj <leocj@hotmail.com>
2020-01-19 00:06:20 -0500

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,11 +1,16 @@
 #ifndef HASH_TABLES_H_
 #define HASH_TABLES_H_
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /*
  * Structs, enums and unions definitions
  * Typedefs
  * Function prototypes
  */
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -37,5 +42,6 @@ typedef struct hash_table_s
 
 
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif /*HASH_TABLES_H_ */
