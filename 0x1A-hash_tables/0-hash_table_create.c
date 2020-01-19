70b81e00a3d3a0894966c3834a7c789653beaeaf
leojj <leocj@hotmail.com>
2020-01-18 23:59:28 -0500

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,10 +12,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = NULL;
-	hash_node_t **array = NULL;
-	unsigned int i = 0;
 
-	(void) i;
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
