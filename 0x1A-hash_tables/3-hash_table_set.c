7c6dc5f292ea40a9209f460f81d1fb3a530565ad
leojj <leocj@hotmail.com>
2020-01-19 02:17:30 -0500

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,13 +12,32 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int ht_key = 0;
-	hash_node_t *new_node = NULL;
+	hash_node_t *new_node = NULL, *temp = NULL;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	/*Check conditions*/
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0 || value == NULL )
 		return (0);
 
+	/*calculate index for the key with the size from hash table*/
 	ht_key = key_index((unsigned char *)key, ht->size);
 
+	/*head of single linked list in position ht_key in the hash table*/
+	temp = ht->array[ht_key];
+
+	/* check if key exist in linked list position ht_key in the hash table */
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			/*value is changed if the key exist*/
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+
+	/*add a new node with key and value in position ht->array[ht_key]*/
 	new_node = add_node(&(ht->array[ht_key]), key, value);
 	if (new_node == NULL)
 		return (0);
@@ -38,13 +57,16 @@ hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
 {
 	hash_node_t *p;
 
+	/*evaluate conditions*/
 	if (head == NULL || key == NULL)
 		return (NULL);
 
+	/*create new node*/
 	p = malloc(sizeof(hash_node_t));
 	if (p == NULL)
 		return (NULL);
 
+	/* store key in new node*/
 	p->key = strdup(key);
 	if (p->key == NULL)
 	{
@@ -52,6 +74,7 @@ hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
 		return (NULL);
 	}
 
+	/* store value in new node*/
 	p->value = strdup(value);
 	if (p->value == NULL)
 	{
@@ -60,6 +83,7 @@ hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
 		return (NULL);
 	}
 
+	/* put new node as head*/
 	p->next = *head;
 	*head = p;
 
