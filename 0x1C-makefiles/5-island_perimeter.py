d9728aa058067d75bc0c9e1368e3eed72519246a
leocjj <leocj@hotmail.com>
2020-02-21 22:21:51 -0500

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,35 @@
 #!/usr/bin/python3
-""""Module to define a rectangle class"""
+""""Function to calculate perimeter of an island"""
+
+def contiguous(grid, i, j):
+    counter = 0
+    if i > 0:
+        if grid[i - 1][j] == 1:
+            counter += 1
+    else:
+        counter += 1
+    if i < len(grid):
+        if grid[i + 1][j] == 1:
+            counter += 1
+    else:
+        counter += 1    
+    if j > 0:
+        if grid[i][j - 1] == 1:
+            counter += 1
+    else:
+        counter += 1
+    if j < len(grid[0]):
+        if grid[i][j + 1] == 1:
+            counter += 1
+    else:
+        counter += 1
+    return counter
 
 def island_perimeter(grid):
-    
+    counter = 0
+    if grid is not None:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    counter += 4 - contiguous(grid, i, j)
+    return counter
