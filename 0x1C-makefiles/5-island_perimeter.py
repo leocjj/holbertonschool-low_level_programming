95f35f869062ad632d8fd7ab88da01a49d72c7d5
leocjj <leocj@hotmail.com>
2020-02-21 22:49:34 -0500

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,22 +3,26 @@
 
 
 def contiguous(grid, i, j):
+    """"Calculates number of contiguous land (1's)"""
+
     counter = 0
     if i > 0:
         if grid[i - 1][j] == 1:
             counter += 1
-    if i < len(grid):
+    if i < len(grid) - 1:
         if grid[i + 1][j] == 1:
             counter += 1
     if j > 0:
         if grid[i][j - 1] == 1:
             counter += 1
-    if j < len(grid[0]):
+    if j < len(grid[0]) - 1:
         if grid[i][j + 1] == 1:
             counter += 1
     return counter
 
 def island_perimeter(grid):
+    """"Function to calculate perimeter of an island"""
+
     counter = 0
     if len(grid) > 0:
         for i in range(len(grid)):
